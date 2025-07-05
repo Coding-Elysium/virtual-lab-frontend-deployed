5f77ed8b8465e571acda323fed8d6bbdfc403f67
John Carlo <abierajc0707@gmail.com>
2025-07-06 06:58:04 +0800

    BASE

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    host: true, 
+    host: true,
     port: 5173,
   },
-})
+  base: process.env.VITE_BASE_PATH || "/virtual-lab-frontend-deployed",
+});
