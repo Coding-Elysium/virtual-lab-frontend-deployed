22c513e86d55f7022d295cd8d6ea8c920410bfdb
John Carlo <abierajc0707@gmail.com>
2025-08-10 13:29:23 +0800

    CARLO

--- src/helper/helper.jsx
+++ src/helper/helper.jsx
@@ -1,2 +1,19 @@
 // export const BASEURL = "http://localhost:5000";
 export const BASEURL = "https://virtual-lab-backend-t0qj.onrender.com";
+
+export const capitalizeWords = (str) => {
+  return str
+    .toLowerCase()
+    .split(" ")
+    .filter((word) => word.length > 0)
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+};
+
+export const capitalizeEachWord = (str) => {
+  return str
+    .replace(/([a-z])([A-Z])/g, "$1 $2") // handle camelCase
+    .split(/[\s_]+/) // split by spaces or underscores
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+    .join(" ");
+};
